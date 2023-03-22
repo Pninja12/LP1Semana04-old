4f5ce4a5caf9f268a50dc01c11854b88a0a84ef0
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-03-22 22:20:57 +0000

    Ex.4 concluído

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,11 +4,18 @@ namespace Power2Method
 {
     class Program
     {
+        private static void PowersOf2UntilN(int n)
+        {
+            for (int i = 1; i <= (1 << n) ; i <<= 1)
+            {
+                Console.WriteLine( i );
+            }
+        }
         private static void PowersOf2Until5()
         {
             for (int i = 1; i <= (1 << 5) ; i <<= 1)
             {
-                Console.WriteLine( i );
+                PowersOf2UntilN(i);
             }
         }
 
